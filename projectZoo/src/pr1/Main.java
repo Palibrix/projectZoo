033526cc6c7c561d41f26de5473d65d071f5f6a0
Palibrix <theopaolo04@gmail.com>
2021-10-22 23:34:19 +0300

    Edited Zoo(An)

--- projectZoo/src/pr1/Main.java
+++ projectZoo/src/pr1/Main.java
@@ -1,13 +1,16 @@
 package pr1;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException {
         Zoo first = new Zoo();
         RandomQuantity random = new RandomQuantity();
 
-        int a = random.random();
-        System.out.println(a);
+        System.out.println("Welcome to out Zoo");
 
+
+//        int a = random.random();
+//        System.out.println(a);
+        first.announce();
 
     }
 }
--- projectZoo/src/pr1/Zoo.java
+++ projectZoo/src/pr1/Zoo.java
@@ -1,14 +1,50 @@
 package pr1;
 
+import java.util.Scanner;
+
 public class Zoo extends Main{
     String name; String location;
-    int price; int quantity;
+    int price; int quantity;  int response;
 
     Zoo(){}
     Zoo(String name, String location, int price, int quantity){
 
     }
 
+
+    void announce() throws InterruptedException {
+        int count = 5;
+        int i;
+        System.out.println("Welcome to our zoo!\nThe show starts in...");
+
+        for(i=5;i > 0 ;i--){
+            System.out.println(count + " seconds");
+            count = count-1;
+            Thread.sleep(1000);
+        }
+        System.out.println(count + " second");
+    }
+
+
+        int pass = 1; //UPDATE PASS FEATURE
+        void phoneCall(){
+            System.out.println("Hi! Youre calling to Zoo. What do you want to find out?");
+            System.out.println("1 - Number of tickets left; 2 - Ticket price; 3 - Hours of work;");
+            Scanner scanner = new Scanner(System.in);
+
+            try {
+                response = scanner.nextInt();
+            }catch(Exception e){
+                System.out.println("Type only number");
+            }
+
+
+            if(response == 1){ //UPDATE PASS FEATURE
+                System.out.println("There are "+"   "+" tickets left.");
+                int pass = 0;
+                System.out.println("Anything else?");
+            }
+        }
 
 
 
