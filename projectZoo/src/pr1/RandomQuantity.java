cc0f58b2ba9da23f0e5914b3b74c34e37f2bae9a
Palibrix <theopaolo04@gmail.com>
2021-10-22 23:34:19 +0300

    Almost completed Phone

--- projectZoo/src/pr1/RandomQuantity.java
+++ projectZoo/src/pr1/RandomQuantity.java
@@ -8,8 +8,14 @@ public class RandomQuantity extends Main {
 
     int random(){
         double i = rd.nextInt(10);
-
         return ((int) i);
     }
-
+    int rdName(){
+        double i = rd.nextInt(5);
+        return ((int) i);
+    }
+    int rdLocation(){
+        double i = rd.nextInt(6);
+        return ((int) i);
+    }
 }
