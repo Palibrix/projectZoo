365e7df26bf70aff339ba4c8ab35ce231d9d88a5
skxlpv <olexadertym0000@gmail.com>
2021-11-05 18:40:39 +0200

    Settled the properties in Event class to future work with it. Changed the TicketBuy class name to Ticket.

--- projectZoo/src/pr1/Event.java
+++ projectZoo/src/pr1/Event.java
@@ -1,7 +1,6 @@
 package pr1;
 
 public class Event extends Main{
-        Human h = new Human();
 
 private enum EventDescriptionEnum {
     NAME,
@@ -9,7 +8,7 @@ private enum EventDescriptionEnum {
     TYPE;
 }
 
-    private EventDescriptionEnum eventDescription;
+    EventDescriptionEnum eventDescription;
 
     Event(EventDescriptionEnum name, EventDescriptionEnum location, EventDescriptionEnum type){
         this.eventDescription = name;
@@ -20,14 +19,14 @@ private enum EventDescriptionEnum {
     void recruitPeople(){
     }
 
-    void regestration() {
-        if (Human.getAge() < 18) {
-            int diff = 18 - Human.getAge();
-            System.out.println("You're not allowed to buy a ticket, come back after " + diff + " years!");
-            menu();
-        } else {
-            System.out.println("You're allowed to buy a ticket. How many do you want to buy?");
-            first.buyTicket();
-        }
-    }
+//    void regestration() {
+//        if (Human.getAge() < 18) {
+//            int diff = 18 - Human.getAge();
+//            System.out.println("You're not allowed to buy a ticket, come back after " + diff + " years!");
+//            menu();
+//        } else {
+//            System.out.println("You're allowed to buy a ticket. How many do you want to buy?");
+//            first.buyTicket();
+//        }
+//    }
 }
--- projectZoo/src/pr1/Ticket.java
+++ projectZoo/src/pr1/Ticket.java
@@ -3,54 +3,54 @@ package pr1;
 import java.util.Scanner;
 
 
-public class TicketBuy extends Main{
-static int quantity;
+class Ticket extends Main{
+    static int quantity;
 
-/*метод,в якому облаштовується випадок, коли користувач купує 0 квитків
-і це ніби розуміється як небажання купувати щось.Тут якраз запитується
-чи він дійсно хоче вийти з купівлі чи ні*/
-  void mindChanging(){
-     Scanner sc = new Scanner(System.in);
-     String buyAnswer = sc.nextLine();
-     buyAnswer = buyAnswer.toLowerCase();
+    /*метод,в якому облаштовується випадок, коли користувач купує 0 квитків
+    і це ніби розуміється як небажання купувати щось.Тут якраз запитується
+    чи він дійсно хоче вийти з купівлі чи ні*/
+    void mindChanging(){
+        Scanner sc = new Scanner(System.in);
+        String buyAnswer = sc.nextLine();
+        buyAnswer = buyAnswer.toLowerCase();
         if(buyAnswer.equals("y")){
-           menu();
+            menu();
         }else if(buyAnswer.equals("n")){
-           buyTicket();
+            buyTicket();
         }else{
-           System.out.println("Please enter [Y/N]");
-           mindChanging();
-    
-  }
-  }
-  //власне метод купівлі квитка, де прописані всі виключення.
-  void buyTicket(){
-     Scanner sc = new Scanner(System.in);
-     int numOfBuyTickets = sc.nextInt();
-     
-       if(numOfBuyTickets>quantity){
-         System.out.println("We don't have so many tickets already!Would you like to buy some less?");
-         buyTicket();
-       }else if(numOfBuyTickets==0){
-         System.out.println("Uhh...Have you changed you're mind?[y/n]");
-         mindChanging();
-       }
-       else if(numOfBuyTickets<0){
-         System.out.println("That's not serious...");
-         buyTicket();
-       }
-       else{
-         System.out.println("There was " + quantity + " tickets");
-           quantity = quantity - numOfBuyTickets;
-         System.out.println("And now there is " + quantity + " of them");
-         System.out.println("You've bought "+ numOfBuyTickets +" ticket(s). Thank you and have fun!");
-           try {
-               first.announce();
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-       }
-     }
+            System.out.println("Please enter [Y/N]");
+            mindChanging();
+
+        }
+    }
+    //власне метод купівлі квитка, де прописані всі виключення.
+    void buyTicket(){
+        Scanner sc = new Scanner(System.in);
+        int numOfBuyTickets = sc.nextInt();
+
+        if(numOfBuyTickets>quantity){
+            System.out.println("We don't have so many tickets already!Would you like to buy some less?");
+            buyTicket();
+        }else if(numOfBuyTickets==0){
+            System.out.println("Uhh...Have you changed you're mind?[y/n]");
+            mindChanging();
+        }
+        else if(numOfBuyTickets<0){
+            System.out.println("That's not serious...");
+            buyTicket();
+        }
+        else{
+            System.out.println("There was " + quantity + " tickets");
+            quantity = quantity - numOfBuyTickets;
+            System.out.println("And now there is " + quantity + " of them");
+            System.out.println("You've bought "+ numOfBuyTickets +" ticket(s). Thank you and have fun!");
+            try {
+                first.announce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
 
