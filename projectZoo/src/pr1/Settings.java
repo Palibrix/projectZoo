f00b6cf302c7e5e7943c50b57c8a5202aea9ca6f
skxlpv <olexadertym0000@gmail.com>
2021-11-07 17:35:44 +0200

    Updated first run detection feature.Now it works on every PC

--- projectZoo/src/pr1/Settings.java
+++ projectZoo/src/pr1/Settings.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Settings extends Main {
-    File file = new File("D:\\КОД\\JAVAPROJECT\\projectZoo\\src\\pr1\\FirstStart");
+    File file = new File("D:\\FirstZooStart");
     boolean firstRun = false;
 
     void firstRunCheck() throws IOException {
@@ -13,10 +13,11 @@ public class Settings extends Main {
             file.createNewFile();
         }
         if (firstRun) {
-            System.out.println("OH, LOOK! A NEW USER! Hello and welcome in our Zoo," +
+            System.out.println("OH, LOOK! A NEW USER! Hello and welcome in our Zoo! Today it's" +
                     " called "+Zoo.names[random.rdName()]+"!");
         } else {
-            System.out.println("Hello and welcome! We're happy to see you again!");
+            System.out.println("Hello and welcome! We're happy to see you in our Zoo AGAIN!" +
+                    "Today it's called "+Zoo.names[random.rdName()]+"!");
         }
     }
 }
