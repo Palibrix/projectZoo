6f7e64111ee16edd4ad247c631460bedc0a198dd
skxlpv <90177691+skxlpv@users.noreply.github.com>
2021-11-09 00:03:32 +0200

    Update Event.java

--- projectZoo/src/pr1/Event.java
+++ projectZoo/src/pr1/Event.java
@@ -10,17 +10,11 @@ public class Event extends Main {
     static String name;
     static String location;
     static String type;
-    Person h = new Person();
 
     Event(String name, String location, String type) {
         this.name = name;
         this.location = location;
         this.type = type;
-    }
-
-    void theFirstStage() {
-        System.out.println("AAAAANDDD NOOOOOOWWWW we're nearly going to start our program! Please, come and pass" +
-                "the registration and who knows, maybe you we'll even be able to see the show!");
     }
 
 }
